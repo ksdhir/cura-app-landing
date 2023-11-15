b0fe37a8ec810e293a597b0cc5cfa74173f47fc3
JB Sinluenam <jb.sinluenam@gmail.com>
2023-11-14 16:25:23 -0800

    set up theme color and font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen ${inter.className} justify-center items-center flex-col`}
-    >
+      className={`flex min-h-screen justify-center items-center flex-col  font-sans`}>
       <h1 className='text-5xl'>This site is a work in progress.</h1>
       <p className='text-xl'>Team Cura 2023</p>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,39 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#09C1CB',
+        primaryDark: '#007F86',
+        secondary: '#F6A8C0',
+        secondaryDark: '#F66490',
+        error: '#EE754E',
+        errorDark: '#EF5B3B',
+        success: '#34CD8D',
+        successDark: '#22AD73',
+        curaWhite: '#F8FFFE',
+        curaGray: '#849A99',
+        curaBlack: '#263130',
+      },
+      fontSize: {
+        sm: '14px',
+        base: '16px',
+        lg: '18px',
+        xl: '22px',
+        '2xl': '24px',
+        '3xl': '26px',
+        '4xl': '28px',
+        '5xl': '32px',
+        '6xl': '60px',
+        black: '82px',
+      },
+      fontFamily: {
+        sans: ['SatoshiMedium', 'Arial', 'sans-serif'],
+
+        SatoshiMedium: 'SatoshiMedium',
+        SatoshiBold: 'SatoshiBold',
+        SatoshiBlack: 'SatoshiBlack',
       },
     },
   },
   plugins: [],
-}
+};
