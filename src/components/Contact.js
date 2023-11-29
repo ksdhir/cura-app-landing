2d63325141f7e6147320c1a92a745da92d2f4bb7
JB Sinluenam <jb.sinluenam@gmail.com>
2023-11-28 22:46:41 -0800

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Image from 'next/image';
 import adPhoto from '../assets/curause.png';
 
 const curaUse = require('../assets/curause.png');
 
-export default function Example() {
-  const [agreed, setAgreed] = useState(false);
+export default function Contact() {
+  const formRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCooldown, setIsCooldown] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsCooldown(true);
+
+      setTimeout(() => {
+        setIsCooldown(false);
+      }, 3000);
+    }, 500);
+
+    formRef.current.reset();
+  };
 
   return (
     <div className='flex flex-col '>
@@ -24,6 +43,8 @@ export default function Example() {
             />
           </div>
           <form
+            ref={formRef}
+            onSubmit={handleSubmit}
             action='#'
             method='POST'
             className='mx-auto  px-6 w-full md:my-16  max-w-2xl'>
@@ -37,8 +58,9 @@ export default function Example() {
                 <div className='mt-2.5'>
                   <input
                     type='text'
-                    name='first-name'
-                    id='first-name'
+                    name='name'
+                    id='name'
+                    required
                     autoComplete='given-name'
                     className='block w-full font-SatoshiMedium rounded-md border-0 px-3.5 py-2 text-primaryDark shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primaryDark sm:text-sm sm:leading-6'
                   />
@@ -55,6 +77,7 @@ export default function Example() {
                     type='email'
                     name='email'
                     id='email'
+                    required
                     autoComplete='email'
                     className='block w-full font-SatoshiMedium rounded-md border-0 px-3.5 py-2 text-primaryDark shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primaryDark sm:text-sm sm:leading-6'
                   />
@@ -71,18 +94,33 @@ export default function Example() {
                     name='message'
                     id='message'
                     rows={4}
-                    className='block w-full rounded-md font-SatoshiMedium border-0 px-3.5 py-2 text-primaryDark shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    required
+                    className='block w-full rounded-md font-SatoshiMedium border-0 px-3.5 py-2 text-primaryDark shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primaryDark sm:text-sm sm:leading-6'
                     defaultValue={''}
                   />
                 </div>
               </div>
             </div>
             <div className='mt-10'>
-              <butto
-                type='submit'
-                className='block w-full rounded-md  bg-primaryDark px-3.5 py-2.5 text-center text-sm font-SatoshiBold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'>
-                Submit
-              </butto>
+              {isLoading ? (
+                <button
+                  type='button'
+                  className='block w-full rounded-md  bg-primaryDark px-3.5 py-2.5 text-center text-sm font-SatoshiBold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'>
+                  <span className='px-3'>Sending...</span>
+                </button>
+              ) : isCooldown ? (
+                <button
+                  type='button'
+                  className='block w-full rounded-md  bg-primaryDark px-3.5 py-2.5 text-center text-sm font-SatoshiBold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'>
+                  <div className='flex justify-center gap-2'>Sent</div>
+                </button>
+              ) : (
+                <button
+                  type='submit'
+                  className='block w-full rounded-md  bg-primaryDark px-3.5 py-2.5 text-center text-sm font-SatoshiBold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary'>
+                  Submit
+                </button>
+              )}
             </div>
           </form>
         </div>
