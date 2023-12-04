0df8fa8f2b1d0d4078aac0229605f93303c7c834
haidoe <haidren.amalia@gmail.com>
2023-12-04 15:43:13 -0800

    chore: add favicon and seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+          {/* Add the link tag for the favicon */}
+          <link rel="icon" href="/favicon-cura.ico" />
+          {/* Other meta tags, stylesheets, etc. */}
+        </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,27 @@ import { Features } from '@/components/Features';
 import Team from '@/components/Team';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>CURA</title>
+
+        <meta
+          name="description"
+          content="CURA is a mobile app that assists in monitoring the health of elderly loved ones. Features heart rate monitoring, fall detection, and location tracking."
+        />
+        <link rel="canonical" href="https://www.cura-app.ca" />
+        {/* Open Graph */}
+        <meta property="og:url" content="https://www.cura-app.ca" />
+        <meta property="og:title" content="CURA: Smart Wearable Health Monitoring App for Elderly Care" />
+        <meta
+          property="og:description"
+          content="CURA is a mobile app that assists in monitoring the health of elderly loved ones. Features heart rate monitoring, fall detection, and location tracking."
+        />
+      </Head>
       <Header />
       <Hero />
       <Intro />
